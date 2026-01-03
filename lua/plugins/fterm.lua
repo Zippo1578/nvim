6b5f2bf1bec9fce4b3eaf06655d0feedd604b3a2
Zippo1578 <133590780+Zippo1578@users.noreply.github.com>
2026-01-03 02:05:40 +0100

    Create fterm.lua

---
+++ lua/plugins/fterm.lua
@@ -0,0 +1,23 @@
+-- ================================================================================================
+-- TITLE : FTerm.nvim
+-- LINKS :
+--   > github : https://github.com/numToStr/FTerm.nvim
+-- ABOUT : Floating terminal plugin for Neovim with easy toggle and configurable shell.
+-- ================================================================================================
+
+return {
+  "numToStr/FTerm.nvim",
+  config = function()
+    local fterm = require("FTerm")
+
+    fterm.setup({
+      cmd = "/bin/bash",   -- change if you use zsh/fish/etc
+      border = "rounded",
+      dimensions = {
+        height = 0.9,
+        width = 0.9,
+      },
+      auto_close = false,
+    })
+  end,
+}
