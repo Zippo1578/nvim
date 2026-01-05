9a5085adb0220b6f89dc9a1a87566e56061008de
Stef Zippo <stefzippo@gmail.com>
2026-01-05 02:44:31 +0100

    linting

--- lua/plugins/ansible.lua
+++ lua/plugins/ansible.lua
@@ -1,46 +1,45 @@
 -- ================================================================================================
 -- TITLE : ansible
 -- ABOUT : Ansible syntax highlighting and linting integration for Neovim.
--- LINKS :
 -- INFO  : "print(vim.fn.system("/home/zippo/.venvs/bin/ansible-lint " .. vim.fn.expand("%")))"
+-- LINKS :
 --   > ansible-vim: https://github.com/pearofducks/ansible-vim
 --   > nvim-lint  : https://github.com/mfussenegger/nvim-lint
 -- ================================================================================================
 return {
-  {
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      local lint = require("lint")
-      local efm = "%f:%l:%c: %m,%f:%l: %m"
+	{
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			local lint = require("lint")
+			local efm = "%f:%l:%c: %m,%f:%l: %m"
 
-      lint.linters.ansible_lint = {
-        name = "ansible-lint",
-        cmd = vim.fn.exepath("ansible-lint"),
-        args = { "-p", "--nocolor" },
-        ignore_exitcode = true,
-        parser = require("lint.parser").from_errorformat(efm, {
-          source = "ansible-lint",
-          severity = vim.diagnostic.severity.INFO,
-        }),
-      }
+			lint.linters.ansible_lint = {
+				name = "ansible-lint",
+				cmd = vim.fn.exepath("ansible-lint"),
+				args = { "-p", "--nocolor" },
+				ignore_exitcode = true,
+				parser = require("lint.parser").from_errorformat(efm, {
+					source = "ansible-lint",
+					severity = vim.diagnostic.severity.INFO,
+				}),
+			}
 
-      lint.linters_by_ft = {
-        ["yaml.ansible"] = { "ansible_lint" },
-        python = { "pylint" }, -- "ruff" is possible
-      }
+			lint.linters_by_ft = {
+				["yaml.ansible"] = { "ansible_lint" },
+				python = { "pylint" }, -- "ruff" is possible
+			}
 
-      vim.api.nvim_create_autocmd("BufWritePost", {
-        callback = function()
-          lint.try_lint()
-        end,
-      })
-    end,
-  },
+			vim.api.nvim_create_autocmd("BufWritePost", {
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+		end,
+	},
 
-  {
-    "pearofducks/ansible-vim",
-    ft = { "yaml.ansible", "ansible" },
-  },
+	{
+		"pearofducks/ansible-vim",
+		ft = { "yaml.ansible", "ansible" },
+	},
 }
-
