4e48121af172566f0874d1ba1b042a0713497bdb
Stef Zippo <stefzippo@gmail.com>
2026-01-10 15:17:43 +0100

    modify lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,53 +13,25 @@ return {
 		"neovim/nvim-lspconfig",
 		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
-			"mason-org/mason.nvim",
-			"mason-org/mason-lspconfig.nvim",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 		},
 		config = function()
 			----------------------
-			-- Mason core
+			-- Mason setup
 			----------------------
 			require("mason").setup()
-
-			----------------------
-			-- LSP server installer
-			----------------------
 			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"ansiblels",
-					"bashls",
-					"yamlls",
-				},
+				ensure_installed = { "lua_ls", "ansiblels", "bashls", "yamlls" },
 				automatic_installation = true,
 			})
-
-			----------------------
-			-- Formatters & Linters installer
-			----------------------
 			require("mason-tool-installer").setup({
-				ensure_installed = {
-					"stylua",
-					"shfmt",
-					"prettierd",
-					"prettier",
-				},
+				ensure_installed = { "stylua", "shfmt", "prettierd", "prettier" },
 			})
 
 			----------------------
-			-- Enable LSP servers
-			----------------------
-			vim.lsp.enable({
-				"lua_ls",
-				"ansiblels",
-				"bashls",
-				"yamlls",
-			})
-
-			----------------------
-			-- Diagnostic Config (your preferred version)
+			-- Diagnostics configuration
 			----------------------
 			vim.diagnostic.config({
 				severity_sort = true,
@@ -76,28 +48,58 @@ return {
 				virtual_text = {
 					source = "if_many",
 					spacing = 2,
-					format = function(diagnostic)
-						local diagnostic_message = {
-							[vim.diagnostic.severity.ERROR] = diagnostic.message,
-							[vim.diagnostic.severity.WARN] = diagnostic.message,
-							[vim.diagnostic.severity.INFO] = diagnostic.message,
-							[vim.diagnostic.severity.HINT] = diagnostic.message,
-						}
-						return diagnostic_message[diagnostic.severity]
+					format = function(d)
+						return d.message
 					end,
 				},
 			})
 
 			----------------------
-			-- Diagnostic keymaps
+			-- Diagnostic keymaps (modern, non-deprecated)
 			----------------------
-			vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show diagnostics" })
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+			vim.keymap.set("n", "gl", function()
+				vim.diagnostic.open_float({ border = "rounded" })
+			end, { desc = "Show diagnostics" })
+
+			vim.keymap.set("n", "[d", function()
+				vim.diagnostic.jump({ count = -1 })
+			end, { desc = "Previous diagnostic" })
+
+			vim.keymap.set("n", "]d", function()
+				vim.diagnostic.jump({ count = 1 })
+			end, { desc = "Next diagnostic" })
+
+			----------------------
+			-- LSP server configurations
+			----------------------
+			local servers = {
+				lua_ls = {}, -- default Lua setup
+				ansiblels = {
+					settings = {
+						ansible = {
+							validation = { enabled = true },
+							lint = { enabled = true },
+							python = { interpreterPath = vim.fn.exepath("python3") },
+						},
+					},
+				},
+				bashls = {},
+				yamlls = {},
+			}
+
+			for server, config in pairs(servers) do
+				-- register the LSP server
+				vim.lsp.config(server, config)
+				-- enable it immediately
+				vim.lsp.enable(server)
+			end
 		end,
 	},
 
-	{ "mason-org/mason.nvim", cmd = "Mason" },
-	{ "mason-org/mason-lspconfig.nvim" },
+	----------------------
+	-- Mason plugins
+	----------------------
+	{ "williamboman/mason.nvim", cmd = "Mason" },
+	{ "williamboman/mason-lspconfig.nvim" },
 	{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
 }
